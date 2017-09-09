0cb1e7f7fc50aafd69443de841a646630010c796
Alex Jackson <alex@alexj.org>
2017-09-09 11:33:33 +0100

    Make Task an asynchronous subclass of Operation

--- Sources/TaskForce/Tasks/Task.swift
+++ Sources/TaskForce/Tasks/Task.swift
@@ -8,8 +8,35 @@
 import Foundation
 
 /**
- `Task` is a direct subclass of `Operation` that implements support for observers and task conditions. Users should
- subclass `Task` to create their own tasks, overriding the `execute()` method to imlpement their task's logic.
+ `Task` is a subclass of `Operation` that implements support for observers and task requirements. Like `Operation`,
+ `Task` should be subclassed to implement the logic of your task. Although `Task`s can be submitted to an
+ `OperationQueue`, the `TaskQueue` class provides the extra logic needed for observers and task requirements to work and
+ should be used instead.
+
+ # Subclassing
+
+ When subclassing, override the `execute()` method to implement the logic of your task. **Do not** call
+ `super.execute()`. `Task` is designed to be an asynchronous subclass of `Operation` (contrary to the `isAsynchronous`
+ property's value). As a result, when `execute()` returns, your task will continue executing. You **must** call
+ `finish(withErrors:)` at some point to inform the `TaskQueue` and any observers that the task has finished.
+
+ # Task Observer
+
+ An observer is any type that conforms to the `TaskObserver` protocol. Observers are notified when a task's `execute()`
+ method is started and when a task's `finish(withErrors:)` method is called. Observers can be added to a task using the
+ `addObserver(_:)` and `addObservers(_:)` methods. Adding the same observer multiple times will result in duplicate
+ notifications from the task. Once added, an observer can not be removed.
+
+ # Task Requirements
+
+ A requirement is any type that conforms to the `TaskRequirement` protocol. Requirements can be added to a task using
+ the `addRequirement(_:)` and `addRequirements(_:)` methods. It is a programmer error (i.e., triggers an assertion) to
+ add requirements once a task is executing or has finished. Once a requirement has been added, it can not be removed.
+
+ Requirements are evaluated before a task is added to a `TaskQueue`. If any requirement isn't satisfied, the task will
+ finish immediately and an array of `RequirementError`s will be passed to any interested observers.
+
+ **Note**: A `Task` with unsatisfied requirements will *not* be marked as cancelled.
  */
 open class Task: Operation {
     // MARK: Associated Types
@@ -24,7 +51,7 @@ open class Task: Operation {
     override final public var isConcurrent: Bool { return isAsynchronous }
 
     private var _ready: Bool = false
-    override final public var isReady: Bool {
+    override final public private(set) var isReady: Bool {
         get {
             return _ready
         }
@@ -33,6 +60,32 @@ open class Task: Operation {
             willChangeValue(forKey: "isReady")
             _ready = newValue
             didChangeValue(forKey: "isReady")
+        }
+    }
+
+    private var _finished = false
+    override final public private(set) var isFinished: Bool {
+        get {
+            return _finished
+        }
+
+        set {
+            willChangeValue(forKey: "isFinished")
+            _finished = newValue
+            didChangeValue(forKey: "isFinished")
+        }
+    }
+
+    private var _executing = false
+    override final public private(set) var isExecuting: Bool {
+        get {
+            return _executing
+        }
+
+        set {
+            willChangeValue(forKey: "isExecuting")
+            _executing = newValue
+            didChangeValue(forKey: "isExecuting")
         }
     }
 
@@ -59,7 +112,7 @@ open class Task: Operation {
     /// Array of requirements that must be satisfied before a task can be executed.
     public private(set) var requirements: [TaskRequirement] = []
 
-	/// Array of accumulated errors due to failing requirements
+  /// Array of accumulated errors due to failing requirements
     private var requirementsErrors: [Error] = []
 
     private func evaluateRequirements(_ requirements: [TaskRequirement], callback: @escaping ([Error]) -> Void) {
@@ -116,12 +169,19 @@ open class Task: Operation {
     }
 
     override final public func start() {
-        super.start()
+        // Return without doing anything if the task has already finished
+        guard isFinished == false else {
+            return
+        }
 
-        if isCancelled {
+        // Finish immediately if the task was cancelled
+        guard isCancelled == false else {
             finish()
             return
         }
+
+        isExecuting = true
+        main()
     }
 
     override final public func main() {
@@ -139,7 +199,11 @@ open class Task: Operation {
         fatalError("\(type(of: self)) must override execute() and not call super")
     }
 
+    /// Finish execution of the Task and notify any observers.
     final public func finish(withErrors errors: [Error] = []) {
+        isExecuting = false
+        isFinished = true
+
         observers.forEach { $0.taskDidFinish(self, withErrors: errors) }
     }
 }
